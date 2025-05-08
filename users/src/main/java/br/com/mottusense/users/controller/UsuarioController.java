72e1870a0b6fca23d34a4e8ed073000defb965a9
joaogananca26 <joaogananca26@gmail.com>
2025-05-08 00:45:33 -0300

    Arrumando direcionamento dos usuários na classe UsuarioController.

--- users/src/main/java/br/com/mottusense/users/controller/UsuarioController.java
+++ users/src/main/java/br/com/mottusense/users/controller/UsuarioController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/usuarios")
 public class UsuarioController {
 
     @Autowired
