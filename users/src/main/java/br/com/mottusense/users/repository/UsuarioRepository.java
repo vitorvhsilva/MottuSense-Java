dfb06d8479a145ffe0e0140674995504e69a6beb
joaogananca26 <joaogananca26@gmail.com>
2025-05-19 20:00:51 -0300

    Removendo erros das classes repositorys

--- users/src/main/java/br/com/mottusense/users/repository/UsuarioRepository.java
+++ users/src/main/java/br/com/mottusense/users/repository/UsuarioRepository.java
@@ -3,6 +3,6 @@ package br.com.mottusense.users.repository;
 import br.com.mottusense.users.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UsuarioRepository extends JpaRepository<Usuario, String> {
-    void deleteAllById(int id);
 }
