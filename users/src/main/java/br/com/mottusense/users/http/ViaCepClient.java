87a7b55cb796edf85a750f9ce0a19b0dfbc92a3c
joaogananca26 <joaogananca26@gmail.com>
2025-05-21 22:01:41 -0300

    Colocando integração da ViaCep com o projeto

--- users/src/main/java/br/com/mottusense/users/http/ViaCepClient.java
+++ users/src/main/java/br/com/mottusense/users/http/ViaCepClient.java
@@ -5,8 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("https://viacep.com.br/ws")
+@FeignClient(name = "viacep-client", url = "https://viacep.com.br/ws")
 public interface ViaCepClient {
-    @GetMapping("/{cep}/json/")
+    @GetMapping("/{cep}/json")
     EnderecoViaCep obterEnderecoDoUsuario(@PathVariable String cep);
 }
