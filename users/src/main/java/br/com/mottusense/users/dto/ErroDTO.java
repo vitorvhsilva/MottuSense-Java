00b624c60a5164b9c864d82e40c2bd1ef44a3426
vitorvhsilva <dndragonbr@gmail.com>
2025-05-22 01:14:46 -0300

    Criação do Global Exception Handler

---
+++ users/src/main/java/br/com/mottusense/users/dto/ErroDTO.java
@@ -0,0 +1,20 @@
+package br.com.mottusense.users.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ErroDTO {
+    private LocalDateTime timestamp = LocalDateTime.now();
+    private String erro;
+    private String mensagem;
+
+    public ErroDTO() {
+    }
+
+    public ErroDTO(String mensagem, String erro) {
+        this.mensagem = mensagem;
+        this.erro = erro;
+    }
+}
---
+++ users/src/main/java/br/com/mottusense/users/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package br.com.mottusense.users.exception;
+
+import br.com.mottusense.users.dto.ErroDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    private ResponseEntity<ErroDTO> handleException(Exception e) {
+        ErroDTO erro = new ErroDTO(HttpStatus.BAD_REQUEST.name(), e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+    }
+
+    @ExceptionHandler(UsuarioNaoEncontradoException.class)
+    private ResponseEntity<ErroDTO> handleUsuarioNaoEncontradoException(Exception e) {
+        ErroDTO erro = new ErroDTO(HttpStatus.NOT_FOUND.name(), e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+    }
+
+    @ExceptionHandler(FilialNaoEncontradaException.class)
+    private ResponseEntity<ErroDTO> handleFilialNaoEncontradaException(Exception e) {
+        ErroDTO erro = new ErroDTO(HttpStatus.NOT_FOUND.name(), e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+    }
+
+    @ExceptionHandler(ConfiguracaoUsuarioNaoEncontradaException.class)
+    private ResponseEntity<ErroDTO> handleConfiguracaoUsuarioNaoEncontradaException(Exception e) {
+        ErroDTO erro = new ErroDTO(HttpStatus.NOT_FOUND.name(), e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+    }
+}
