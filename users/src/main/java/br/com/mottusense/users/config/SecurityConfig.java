15f82084c47e296263c571c379f9b7b573dc5c4c
joaogananca26 <joaogananca26@gmail.com>
2025-08-23 14:42:15 -0300

    Criando a classe de configuração do spring security

---
+++ users/src/main/java/br/com/mottusense/users/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package br.com.mottusense.users.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(
+                        authorizeConfig -> {
+                            authorizeConfig.requestMatchers("/usuarios").permitAll();
+                            authorizeConfig.requestMatchers("/logout").permitAll();
+                            authorizeConfig.anyRequest().authenticated();
+                        })
+                .formLogin(form -> form
+                        .defaultSuccessUrl("/usuarios   ", true) // Caso o login funcione, irá para esta página
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll()
+                )
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("teste")
+                .roles("ADMIN, USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+}
+
