6281fb7be7e4ddf987f4fa4928ac69f24d9de15e
João Ganança <93109162+Joaogananca26@users.noreply.github.com>
2025-09-21 19:12:53 -0300

    Adicionando o botão para logout

--- users/src/main/java/br/com/mottusense/users/config/SecurityConfig.java
+++ users/src/main/java/br/com/mottusense/users/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                 )
                 .logout(logout -> logout
                         .logoutUrl("/logout")
-                        .logoutSuccessUrl("/autenticacao/logout")
+                        .logoutSuccessUrl("/logout")
                         .permitAll()
                 )
                 .build();
